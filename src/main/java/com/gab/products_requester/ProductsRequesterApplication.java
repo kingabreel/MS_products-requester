a2b65ed07f1cdf7598571cd9ce2ce605beacb98b
gabriel <gabriel.donascimento@jala.university>
2025-06-29 00:07:28 -0300

    first commit

---
+++ src/main/java/com/gab/products_requester/ProductsRequesterApplication.java
@@ -0,0 +1,13 @@
+package com.gab.products_requester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsRequesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductsRequesterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gab/products_requester/ProductsRequesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gab.products_requester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsRequesterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
